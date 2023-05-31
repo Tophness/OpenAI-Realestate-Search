04212ff15776425f0b4aac4bcc35e10b26535c90
Chris Malone <chrismrulz@hotmail.com>
2023-06-01 06:38:54 +1000

    fix trimmedData structure

--- server.js
+++ server.js
@@ -18,73 +18,74 @@ app.use('/', proxy('https://services.realestate.com.au/services/listings/search'
     if (req.url.indexOf('?query=') !== -1) {
       const data = JSON.parse(proxyResData.toString('utf8'));
       if (data.tieredResults) {
-        const totalResultsCount = data.tieredResults.totalResultsCount;
-        const cpage = data.resolvedQuery.page;
-        const psize = data.resolvedQuery.pageSize;
-        let trimmedData = data.tieredResults[0];
-        trimmedData['totalListings'] = totalResultsCount;
-        trimmedData['currentPage'] = cpage;
-        trimmedData['pageSize'] = psize;
-        for (const id in trimmedData.results) {
-         if (trimmedData.results.hasOwnProperty(id)) {
-           delete trimmedData.results[id].standard;
-           delete trimmedData.results[id].midtier;
-           delete trimmedData.results[id].lister;
-           delete trimmedData.results[id].featured;
-           delete trimmedData.results[id]['_links'];
-           delete trimmedData.results[id].signature;
-           delete trimmedData.results[id].channel;
-           delete trimmedData.results[id].advertising;
-           delete trimmedData.results[id].showAgencyLogo;
-           delete trimmedData.results[id].listers;
-           delete trimmedData.results[id].productDepth;
-           delete trimmedData.results[id].calculator;
-           delete trimmedData.results[id].address.subdivisionCode;
-           delete trimmedData.results[id].address.showAddress;
-           delete trimmedData.results[id].address.locality;
-           delete trimmedData.results[id].agency;
-           delete trimmedData.results[id].isSoldChannel;
-           delete trimmedData.results[id].isBuyChannel;
-           delete trimmedData.results[id].signatureProject;
-           delete trimmedData.results[id].listingId;
-           delete trimmedData.results[id].classicProject;
-           delete trimmedData.results[id].agencyListingId;
-           delete trimmedData.results[id].mainImage;
-           delete trimmedData.results[id].dateAvailable.dateDisplay;
-           delete trimmedData.results[id].modifiedDate;
-           delete trimmedData.results[id].isRentChannel;
-           delete trimmedData.results[id].applyOnline;
+        let trimmedData = data.tieredResults[0].results;
+        let returnJSON = {
+          totalListings: data.tieredResults.totalResultsCount,
+          currentPage: data.resolvedQuery.page,
+          pageSize: data.resolvedQuery.pageSize,
+          results: null
+        };
+        for (const id in trimmedData) {
+         if (trimmedData.hasOwnProperty(id)) {
+           delete trimmedData[id].standard;
+           delete trimmedData[id].midtier;
+           delete trimmedData[id].lister;
+           delete trimmedData[id].featured;
+           delete trimmedData[id]['_links'];
+           delete trimmedData[id].signature;
+           delete trimmedData[id].channel;
+           delete trimmedData[id].advertising;
+           delete trimmedData[id].showAgencyLogo;
+           delete trimmedData[id].listers;
+           delete trimmedData[id].productDepth;
+           delete trimmedData[id].calculator;
+           delete trimmedData[id].address.subdivisionCode;
+           delete trimmedData[id].address.showAddress;
+           delete trimmedData[id].address.locality;
+           delete trimmedData[id].agency;
+           delete trimmedData[id].isSoldChannel;
+           delete trimmedData[id].isBuyChannel;
+           delete trimmedData[id].signatureProject;
+           delete trimmedData[id].listingId;
+           delete trimmedData[id].classicProject;
+           delete trimmedData[id].agencyListingId;
+           delete trimmedData[id].mainImage;
+           delete trimmedData[id].dateAvailable.dateDisplay;
+           delete trimmedData[id].modifiedDate;
+           delete trimmedData[id].isRentChannel;
+           delete trimmedData[id].applyOnline;
          }
         }
         const imgParam = req.url.match('[?&]images=([^&]+)');
         if (imgParam) {
           const numImages = parseInt(imgParam[1]);
           if(numImages > 0){
-            for (let key in trimmedData.results) {
-              if (trimmedData.results.hasOwnProperty(key)) {
-                const images = trimmedData.results[key].images;
+            for (let key in trimmedData) {
+              if (trimmedData.hasOwnProperty(key)) {
+                const images = trimmedData[key].images;
                 if (Array.isArray(images) && images.length > numImages) {
-                  trimmedData.results[key].images = images.slice(0, numImages);
+                  trimmedData[key].images = images.slice(0, numImages);
                 }
               }
             }
           }
           else{
-            for (let key in trimmedData.results) {
-              if (trimmedData.results.hasOwnProperty(key)) {
-                delete trimmedData.results[key].images;
+            for (let key in trimmedData) {
+              if (trimmedData.hasOwnProperty(key)) {
+                delete trimmedData[key].images;
               }
             }
           }
         }
         else{
-            for (let key in trimmedData.results) {
-              if (trimmedData.results.hasOwnProperty(key)) {
-                delete trimmedData.results[key].images;
+            for (let key in trimmedData) {
+              if (trimmedData.hasOwnProperty(key)) {
+                delete trimmedData[key].images;
               }
             }
         }
-        return JSON.stringify(trimmedData);
+        returnJSON.results = trimmedData;
+        return JSON.stringify(returnJSON);
       }
       else{
          return proxyResData;
